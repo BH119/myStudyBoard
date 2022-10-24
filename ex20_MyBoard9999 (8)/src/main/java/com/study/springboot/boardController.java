d18d838f7cfc30a3f596b8fcbea809aec7fa871e
bonghyun <94823572+BH119@users.noreply.github.com>
2022-10-24 20:52:45 +0900

    Revert "jskdffjkdsjkfdsfjksdjklfds"
    This reverts commit 835e6c343d4026e1bf8932aaf7be1168e7e029a6.

--- ex20_MyBoard9999 (8)/src/main/java/com/study/springboot/boardController.java
+++ ex20_MyBoard9999 (8)/src/main/java/com/study/springboot/boardController.java
@@ -20,13 +20,11 @@ import org.springframework.web.multipart.MultipartFile;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.study.springboot.dao.boardDao;
-import com.study.springboot.dao.chatDao;
 import com.study.springboot.dao.imgBoardDao;
 import com.study.springboot.dao.membersDao;
 import com.study.springboot.dao.one2oneDao;
 import com.study.springboot.dao.membersDao;
 import com.study.springboot.dao.replyDao;
-import com.study.springboot.dto.chatDto;
 import com.study.springboot.dto.imgBoardDto;
 import com.study.springboot.dto.membersDto;
 import com.study.springboot.dto.replyDto;
@@ -82,8 +80,6 @@ public class boardController {
 	one2onePageService iOne2onePageService;
 	@Autowired
 	one2oneSearchService iOne2oneSearchService;
-	@Autowired
-	chatDao iChatDao;
 	
 	//메인페이지 폼으로
 	@RequestMapping("/")
@@ -497,6 +493,7 @@ public class boardController {
 	//이미지글쓰기폼
 	@RequestMapping("imgBoardWrite")
 	public String imgBoardWrite( ) {
+		System.out.println("ddd");
 		return "imgBoard/imgBoardWrite";
 	}
 	
@@ -632,32 +629,7 @@ public class boardController {
 	}
 	
 	
-	//채팅방으로~
-	@RequestMapping("/chat")
-	public String chat() {
-		return "/chat";
-	}
 	
-	
-	//채팅저장
-	@RequestMapping("/chatAction")
-	public String chatAction(
-			@RequestParam("member_nameVal")String member_nameVal,
-			@RequestParam("chat_contentVal")String chat_contentVal,
-			Model model) {
-			
-		
-		iChatDao.chatWrite(member_nameVal, chat_contentVal);
-		List<chatDto> list = iChatDao.chatList();
-		
-		model.addAttribute("list", list);
-		
-		
-		System.out.println(list);
-		System.out.println(member_nameVal);
-		System.out.println(chat_contentVal);
-		return "0";
-	}
 	
 	
 	
